1ba1fd6ad55cba0babb9511b645e28bf8ccd3888
florencia chao <fchao@itba.edu.ar>
2022-03-12 12:18:31 -0300

    avances

--- TP1/algorithms/search.py
+++ TP1/algorithms/search.py
@@ -1,4 +1,11 @@
+from logging import root
 from solution import GOAL_STATE
+from solution import possible_moves
+from solution import next
+
+
+def get_depth(nodes):
+    return nodes.depth
 
 def search(root_node, sort_key = get_depth, reverse = True):
     A = [root_node]
@@ -7,21 +14,21 @@ def search(root_node, sort_key = get_depth, reverse = True):
 
     while len(F) > 0:
         node = F.pop(0)
+        print ("NODE ", node.matrix)
         Ex.append(node)
 
         if node.__eq__(GOAL_STATE):
-            return "encontrado"
+            print ("encontrado")
+            return
         
-        moves = possible_moves(node.matrix, node.blankSpace)
+        moves = possible_moves(node.matrix, node.blankspace)
 
         for move in moves:
-            if move not in Ex:
-                F.append(move)
-                A.append(move)
-    
-    F.sort(sort_key, reverse)
-    
-    return "no encontrado"
+            nextNode = next(node.matrix, node.blankspace, move, node)
+            if nextNode not in Ex:
+                F.append(nextNode)
+                A.append(nextNode)
+        F.sort(key= sort_key, reverse = reverse)
 
-def get_depth(nodes):
-    return nodes.depth
+    print("no encontrado")
+    return
