55b9a2d9bb2400071f973d2a43ea9d49b3b349c1
arfernandez2000 <arfernandez@itba.edu.ar>
2022-03-29 10:24:25 -0300

    Co-authored-by: Florencia Chao <florchao@users.noreply.github.com> Co-authored-by: Faustino Maggioni Duffy <maggioniduffy@users.noreply.github.com>

---
+++ TP2/algorithms/mutation.py
@@ -0,0 +1,4 @@
+from TP2.backpack import Chromosome
+
+def mutation(chromosome: Chromosome, p):
+    print(p)
---
+++ TP2/algorithms/selection.py
@@ -0,0 +1,17 @@
+def elite():
+    i = 0
+
+def ruleta():
+    i = 0
+
+def rank():
+    i = 0
+
+def tournament():
+    i = 0
+
+def boltzman():
+    i = 0
+
+def truncated():
+    i = 0
