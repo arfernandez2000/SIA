9e0403cba73b688f62bdf351b16bb741bf35a029
maggioniduffy <faustino.maggioni@est.fi.uncoma.edu.ar>
2022-05-07 20:31:34 -0300

    title added to plot in ex3.2

--- TP3/plot_accuracies.py
+++ TP3/plot_accuracies.py
@@ -3,12 +3,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #No testeado
-def plot_acc(epochs, training, test):
+def plot_acc(epochs, training, test, title=''):
     plt.style.use('default')
     fig, ax = plt.subplots()
     y_max = np.amax(np.array([np.amax(training),np.amax(test)]))
     plt.ylabel('Accuracie')
     plt.xlabel('Epochs')
+    plt.title(title)
     length = len(epochs)
     ax.set(xlim=(0,length), xticks=np.arange(0,length, length/10),
             ylim=(0,y_max + 0.1), yticks=np.arange(0,y_max + 0.1, y_max/10))
--- TP3/plot_errors.py
+++ TP3/plot_errors.py
@@ -19,13 +19,14 @@ def plot():
     ax.plot(x,errors)
     plt.show()
 
-def plot_list_error(errs):
+def plot_list_error(errs, title=''):
     plt.style.use('default')
     fig, ax = plt.subplots()
     length = len(errs)
     y_max = np.amax(errs)
     plt.ylabel('Error')
     plt.xlabel('Iteraciones')
+    plt.title(title)
     x = np.arange(0, length)
     ax.set(xlim=(0,length), xticks=np.arange(0,length, length/10),
             ylim=(0,y_max + 1), yticks=np.arange(0,y_max + 1, y_max/10))
