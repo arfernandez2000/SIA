63b884edc1a4a64b63463f5455b4d25359002814
maggioniduffy <faustino.maggioni@est.fi.uncoma.edu.ar>
2022-05-17 16:37:43 -0300

    Updating neighbors ok, doubts in x,data and weights

--- TP4/kohonen/kohonen.py
+++ TP4/kohonen/kohonen.py
@@ -3,18 +3,29 @@ from get_data import data, raw_data
 #Paso Inicial: Inicializo valores
 k = 5 # Revisar
 p = len(raw_data)
+countries = raw_data.values[:,0]
 n = data.shape[1]
-
+print(data.shape)
 def set_init_weights():
   w = []
-  for _ in range(1,p):
+  for _ in range(0,p):
     wi = np.random.rand(n)
     w.append(wi)
   return np.array(w)
 
-def kohonen(init_radius = 1, init_learn_rate = 0.5, max_epochs = 100):
+def update_neighborhood_weight(radius, w_k):
+  #neu - neuk ?
+  umbral = np.sum(data[w_k])
+  res = []
+  for i in range(len(data)):
+    if np.abs(np.sum(data[i]) - umbral) < radius:
+      res.append(i)
+  return np.array(res)
+
+def kohonen(init_radius = np.sqrt(2), init_learn_rate = 0.5, max_epochs = 100):
   #Paso Inicial
   weights = set_init_weights()
+  print(weights.shape)
   radius = init_radius
   learn_rate = init_learn_rate
   t = 1
@@ -24,11 +35,19 @@ def kohonen(init_radius = 1, init_learn_rate = 0.5, max_epochs = 100):
     #Paso 1: Selecciono un registro de entrada Xp
     x_index = np.random.choice(range(data.shape[0]))
     x = data[x_index]
+    print(x.shape,data.shape,weights.shape)
     #Paso 2: Encontrar la neurona ganadora
+    # Xp se refiere a todo el conjunto o a un registro elegido de Xp?
     aux = []
     for w in weights:
-      aux.append(x - w)
+      aux.append(np.sum(np.abs(data - w))) # PREGUNTAR si data o x
+    aux = np.array(aux)
     w_k = np.argmin(aux)
     #Paso 3: Actualizar los pesos de las neuronas vecinas
+    n_k = update_neighborhood_weight(radius,w_k)
+    for j in range(len(weights)):
+      if (j in n_k):
+        print(data.shape,weights[j].shape)
+        weights[j] = weights[j] + learn_rate * (data[j]-weights[j])
     t += 1
 kohonen()
--- TP4/kohonen/plot.py
+++ TP4/kohonen/plot.py
@@ -29,3 +29,58 @@ plt.style.use('default')
 # ax.imshow(Z)
 
 # plt.show()
+
+import seaborn as sns
+sns.set_theme(style="ticks")
+
+# Load the planets dataset and initialize the figure
+planets = sns.load_dataset("planets")
+g = sns.JointGrid(data=planets, x="year", y="distance", marginal_ticks=True)
+
+# Set a log scaling on the y axis
+g.ax_joint.set(yscale="log")
+
+# Create an inset legend for the histogram colorbar
+cax = g.figure.add_axes([.15, .55, .02, .2])
+
+# Add the joint and marginal histogram plots
+g.plot_joint(
+    sns.histplot, discrete=(True, False),
+    cmap="light:#03012d", pmax=.8, cbar=True, cbar_ax=cax
+)
+g.plot_marginals(sns.histplot, element="step", color="#03012d")
+
+import seaborn as sns
+sns.set_theme(style="whitegrid")
+
+# Load the brain networks dataset, select subset, and collapse the multi-index
+df = sns.load_dataset("brain_networks", header=[0, 1, 2], index_col=0)
+
+used_networks = [1, 5, 6, 7, 8, 12, 13, 17]
+used_columns = (df.columns
+                  .get_level_values("network")
+                  .astype(int)
+                  .isin(used_networks))
+df = df.loc[:, used_columns]
+
+df.columns = df.columns.map("-".join)
+
+# Compute a correlation matrix and convert to long-form
+corr_mat = df.corr().stack().reset_index(name="correlation")
+
+# Draw each cell as a scatter point with varying size and color
+g = sns.relplot(
+    data=corr_mat,
+    x="level_0", y="level_1", hue="correlation", size="correlation",
+    palette="vlag", hue_norm=(-1, 1), edgecolor=".7",
+    height=10, sizes=(50, 250), size_norm=(-.2, .8),
+)
+
+# Tweak the figure to finalize
+g.set(xlabel="", ylabel="", aspect="equal")
+g.despine(left=True, bottom=True)
+g.ax.margins(.02)
+for label in g.ax.get_xticklabels():
+    label.set_rotation(90)
+for artist in g.legend.legendHandles:
+    artist.set_edgecolor(".7")
